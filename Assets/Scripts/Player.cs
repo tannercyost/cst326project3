4b988cbcbb04bae62b806b87426e010c7e84b0ff
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-02-26 20:57:13 -0800

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    GameManager gm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // NYI
     }
 }
