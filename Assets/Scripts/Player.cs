9d1bc10188eae0303a68906de9851a408a91e52d
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-02-26 21:07:46 -0800

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     GameManager gm;
+    [SerializeField] public float speed;
     // Start is called before the first frame update
     void Start()
     {
         gm = GetComponent<GameManager>();
+        speed = 5.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (Input.GetAxis("Horizontal") == 1)
+        //{
+        //    float translation = Input.GetAxis("Horizontal") * speed;
+        //    translation *= Time.deltaTime;
+        //    transform.Translate(translation, 0, 0);
+        //}
+        //if (Input.GetAxis("Horizontal") == -1)
+        //{
+        //    float translation = Input.GetAxis("Horizontal") * speed;
+        //    translation *= Time.deltaTime;
+        //    transform.Translate(translation, 0, 0);
+        //}
+        //if (Input.GetKeyDown(KeyCode.LeftShift))
+        //{
+        //    speed = 10.0f;
+        //} 
+        //else
+        //{
+        //    speed = 5.0f;
+        //}
+        float translation = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        transform.Translate(translation, 0, 0);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
